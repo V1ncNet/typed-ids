f5c7f541585fd70dc7ea61e101fba45ee21e28a1
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-01-30 23:50:00 +0100

    Verify (de-)serialization of identifiable object

---
+++ jackson/src/test/java/de/vinado/library/identifier/IdentifiableSerdeTest.java
@@ -0,0 +1,58 @@
+package de.vinado.library.identifier;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.vinado.library.identifier.string.StringIdentifier;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Vincent Nadoll
+ */
+class IdentifiableSerdeTest {
+
+    private static ObjectMapper mapper;
+
+    @BeforeAll
+    static void beforeAll() {
+        mapper = new ObjectMapper()
+            .findAndRegisterModules();
+    }
+
+    @Test
+    void serializingIdentifiable_shouldContainIdentifierAsId() throws JsonProcessingException {
+        Identifiable.Id identifier = new Identifiable.Id("foo");
+        Identifiable identifiable = new Identifiable(identifier);
+
+        String json = mapper.writeValueAsString(identifiable);
+
+        assertEquals("{\"id\":\"foo\"}", json);
+    }
+
+    @Test
+    void deserializingIdentifiable_shouldMapIdFieldToIdentifier() throws JsonProcessingException {
+        Identifiable identifiable = mapper.readValue("{\"id\":\"foo\"}", Identifiable.class);
+
+        assertEquals("foo", identifiable.getId().toString());
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private static class Identifiable {
+
+        private Id id;
+
+        private static final class Id extends StringIdentifier {
+
+            Id(String value) {
+                super(value);
+            }
+        }
+    }
+}
