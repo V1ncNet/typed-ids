a4ea184af84303a0c051b445999c4f73a34a593c
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-01-31 01:31:15 +0100

    Provide instantiation via reflection

---
+++ api/src/main/java/de/vinado/library/identifier/reflection/ReflectionUtils.java
@@ -0,0 +1,58 @@
+package de.vinado.library.identifier.reflection;
+
+import de.vinado.library.identifier.Identifier;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+
+/**
+ * Utility methods for {@link Identifier}s.
+ *
+ * @author Vincent Nadoll
+ */
+public final class ReflectionUtils {
+
+    private ReflectionUtils() {
+        throw new UnsupportedOperationException("Utility class may not be instantiated");
+    }
+
+    public static <T> T instantiate(Constructor<T> constructor, Object value) throws IdentifierInstantiationException {
+        assertCompatibility(constructor, "The class to be constructed must be a subtype of Identifier");
+
+        try {
+            makeAccessible(constructor);
+            return constructor.newInstance(value);
+        } catch (InvocationTargetException e) {
+            Throwable targetException = e.getTargetException();
+            throw new IdentifierInstantiationException(constructor, "Constructor threw exception", targetException);
+        } catch (InstantiationException e) {
+            throw new IdentifierInstantiationException(constructor, "Is it an abstract class?", e);
+        } catch (IllegalAccessException e) {
+            throw new IdentifierInstantiationException(constructor, "Is the constructor accessible?", e);
+        } catch (IllegalArgumentException e) {
+            throw new IdentifierInstantiationException(constructor, "Illegal arguments for constructor", e);
+        }
+    }
+
+    private static void assertCompatibility(Constructor<?> constructor, String message) {
+        if (!Identifier.class.isAssignableFrom(constructor.getDeclaringClass())) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    /**
+     * Make the given constructor accessible, explicitly setting it accessible if necessary. The {@code
+     * setAccessible(true)} method is only called when actually necessary, to avoid unnecessary conflicts.
+     *
+     * @param ctor the constructor to make accessible
+     * @author jhoeller
+     */
+    @SuppressWarnings("deprecation")
+    private static void makeAccessible(Constructor<?> ctor) {
+        if ((!Modifier.isPublic(ctor.getModifiers()) || !Modifier.isPublic(ctor.getDeclaringClass().getModifiers()))
+            && !ctor.isAccessible()) {
+            ctor.setAccessible(true);
+        }
+    }
+}
---
+++ api/src/test/java/de/vinado/library/identifier/reflection/ReflectionUtilsTest.java
@@ -0,0 +1,105 @@
+package de.vinado.library.identifier.reflection;
+
+import de.vinado.library.identifier.Identifier;
+import de.vinado.library.identifier.string.StringIdentifier;
+import de.vinado.library.identifier.uuid.UuidIdentifier;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.lang.reflect.Constructor;
+import java.util.UUID;
+
+import static de.vinado.library.identifier.reflection.ReflectionUtils.instantiate;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * @author Vincent Nadoll
+ */
+class ReflectionUtilsTest {
+
+    @Test
+    void instantiatingFixedValueIdentifier_shouldThrowException() throws NoSuchMethodException {
+        Constructor<FixedValueIdentifier> constructor = FixedValueIdentifier.class.getDeclaredConstructor();
+
+        assertThrows(IdentifierInstantiationException.class, () -> instantiate(constructor, "foo"));
+    }
+
+    @Test
+    void instantiatingWithIncompatibleType_shouldThrowException() throws NoSuchMethodException {
+        Constructor<UuidIdentifier> constructor = UuidIdentifier.class.getDeclaredConstructor(UUID.class);
+
+        assertThrows(IdentifierInstantiationException.class, () -> instantiate(constructor, "foo"));
+    }
+
+    @Test
+    void instantiatingConstrained_shouldThrowException() throws NoSuchMethodException {
+        Constructor<ConstrainedIdentifier> constructor = ConstrainedIdentifier.class.getDeclaredConstructor(String.class);
+
+        assertThrows(IdentifierInstantiationException.class, () -> instantiate(constructor, "1"));
+    }
+
+    @Test
+    void instantiatingIdentifier_shouldThrowException() throws NoSuchMethodException {
+        Constructor<AbstractStringIdentifier> constructor = AbstractStringIdentifier.class.getDeclaredConstructor(String.class);
+
+        assertThrows(IdentifierInstantiationException.class, () -> instantiate(constructor, "foo"));
+    }
+
+    @Test
+    void instantiatingArbitraryOneArgConstructor_shouldThrowException() throws NoSuchMethodException {
+        Constructor<String> constructor = String.class.getDeclaredConstructor(String.class);
+
+        assertThrows(IllegalArgumentException.class, () -> instantiate(constructor, "foo"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("constructors")
+    void instantiatingUuidIdentifier_shouldNotBeNull(Constructor<?> oneArgConstructor, Object value) {
+        Identifier<?> identifier = (Identifier<?>) instantiate(oneArgConstructor, value);
+
+        assertNotNull(identifier);
+    }
+
+    static Arguments[] constructors() throws NoSuchMethodException {
+        return new Arguments[]{
+            Arguments.arguments(UuidIdentifier.class.getDeclaredConstructor(UUID.class), UUID.randomUUID()),
+            Arguments.arguments(StringIdentifier.class.getDeclaredConstructor(String.class), "foo"),
+        };
+    }
+
+
+    private static abstract class AbstractStringIdentifier extends Identifier<String> {
+
+        public AbstractStringIdentifier(String value) {
+            super(value);
+        }
+    }
+
+
+    private static class ConstrainedIdentifier extends StringIdentifier {
+
+        public ConstrainedIdentifier(String value) {
+            super(nonNumeric(value));
+        }
+
+        private static String nonNumeric(String value) {
+            try {
+                Long.parseLong(value);
+                throw new IllegalArgumentException();
+            } catch (NumberFormatException e) {
+                return value;
+            }
+        }
+    }
+
+
+    private static class FixedValueIdentifier extends StringIdentifier {
+
+        public FixedValueIdentifier() {
+            super("foo");
+        }
+    }
+}
